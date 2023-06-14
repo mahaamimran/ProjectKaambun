aef918438754421a9e6f09971bd4c816e8a3ca17
mahaamimran <mahaamimran@gmail.com>
2023-06-15 00:10:50 +0500

    scrollwheel

--- lib/testwidget.dart
+++ lib/testwidget.dart
@@ -1,4 +1,6 @@
 // ignore_for_file:  prefer_const_literals_to_create_immutables, file_names, library_private_types_in_public_api, avoid_print
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:project/Hours.dart';
 import 'package:project/Minutes.dart';
@@ -104,7 +106,6 @@ class _TestWidgetState extends State<TestWidget> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   List<int> hoursList = List<int>.generate(12, (index) => index + 1);
   List<int> minutesList = List<int>.generate(60, (index) => index);
- 
 
   // snackbar
   void _showSnackBar(String message) {
@@ -123,12 +124,261 @@ class _TestWidgetState extends State<TestWidget> {
       backgroundColor: const Color(0xff212327),
       body: Column(
         children: [
-          Padding(
-            padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.02),
-            child: Row(
+          // add more
+          Row(
+            children: [
+              // task name
+              Expanded(
+                child: TextField(
+                  controller: taskNameController,
+                  enabled: isEditing,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Task Name',
+                    contentPadding: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * 0.04,
+                      bottom: MediaQuery.of(context).size.width * 0.01,
+                      top: MediaQuery.of(context).size.width * 0.05,
+                      right: MediaQuery.of(context).size.width * 0.03,
+                    ),
+                    hintStyle: const TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    fontSize: MediaQuery.of(context).size.width * 0.04,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Cupertino",
+                    letterSpacing: 0,
+                  ),
+                ),
+              ),
+
+              // toggles color of pen icon
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isEditing = !isEditing;
+                    penColor = isEditing
+                        ? const Color.fromRGBO(203, 124, 229, 1)
+                        : Colors.white;
+                  });
+                  isEditing
+                      ? _showSnackBar("Editing Enabled")
+                      : _showSnackBar("Editing Disabled");
+                },
+                child: Image.asset(
+                  'assets/editpen.png',
+                  color: penColor,
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.02,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.width * 0.02,
+          ),
+          // scrollable list for alarm
+          Expanded(
+            child: ListWheelScrollView(
+              itemExtent: 50, // Height of each item
               children: [
+                // List of items within the scroll view
+                Text(
+                  'Item 1',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: MediaQuery.of(context).size.width * 0.04,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Cupertino",
+                    letterSpacing: 0,
+                  ),
+                ),
+                Text(
+                  'Item 1',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: MediaQuery.of(context).size.width * 0.04,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Cupertino",
+                    letterSpacing: 0,
+                  ),
+                ),
+                Text(
+                  'Item 1',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: MediaQuery.of(context).size.width * 0.04,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Cupertino",
+                    letterSpacing: 0,
+                  ),
+                ),
+                // Add more items as needed
               ],
             ),
+          ),
+
+          // alarm ke neechay
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.02),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // repeat making it left
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.02,
+                      ),
+                      Text(
+                        "Repeat",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: MediaQuery.of(context).size.width * 0.05,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                  // space between repeat and weekdays
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * 0.03,
+                  ),
+                  // weekdays
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      for (var weekday in [
+                        'Mon',
+                        'Tue',
+                        'Wed',
+                        'Thu',
+                        'Fri',
+                        'Sat',
+                        'Sun'
+                      ])
+                        CustomButton(weekday),
+                    ],
+                  ),
+
+                  // space between weekdays and subtasks
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * 0.05,
+                  ),
+                  // subtasks
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.02,
+                      ),
+                      Text(
+                        "Sub tasks",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: MediaQuery.of(context).size.width * 0.05,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                  // none
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: subTaskNameController,
+                          enabled: isEditing,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'None',
+                            contentPadding: EdgeInsets.only(
+                              left: MediaQuery.of(context).size.width * 0.02,
+                              right: MediaQuery.of(context).size.width * 0.02,
+                              top: MediaQuery.of(context).size.width * 0.02,
+                              bottom: MediaQuery.of(context).size.width * 0.02,
+                            ),
+                            hintStyle: TextStyle(
+                              color: isEditing ? Colors.white : Colors.grey,
+                            ),
+                          ),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: MediaQuery.of(context).size.width * 0.04,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Cupertino",
+                            letterSpacing: 0,
+                          ),
+                        ),
+                      ),
+                      Image.asset(
+                        "assets/rightarrow.png",
+                        width: MediaQuery.of(context).size.width * 0.07,
+                        scale: 0.7,
+                      ),
+                    ],
+                  ),
+
+                  // length of task
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.02,
+                      ),
+                      Text(
+                        "Length of task",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: MediaQuery.of(context).size.width * 0.05,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                  // add slider here for length of task
+                  Slider(
+                    value: sliderValue,
+                    min: 0.0,
+                    max: 100.0,
+                    onChanged: (newValue) {
+                      setState(() {
+                        sliderValue = newValue;
+                      });
+                    },
+                    activeColor: const Color(0xffc847f4),
+                    thumbColor: Colors.white,
+                    inactiveColor: const Color.fromRGBO(77, 77, 77, 0.9),
+                  ),
+                  // time
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          left: MediaQuery.of(context).size.width *
+                              0.04), // Adjust the left padding as needed
+                      child: Text(
+                        sliderValue.round() >= 60
+                            ? '${(sliderValue.round() ~/ 60).toString()} hours ${(sliderValue.round() % 60).toString()} minutes'
+                            : '${sliderValue.round().toString()} minutes',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                  // add more code here
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.width * 0.25,
           ),
         ],
       ),
